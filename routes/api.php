dd1255234a09ad583d31828af81527369512f5d5
BroakenMedia <joe@broakenmedia.co.uk>
2024-02-08 01:00:34 +0000

    Add routes.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\GetBeerController;
+use App\Http\Controllers\Api\GetBeersController;
+use App\Http\Controllers\Api\GetRandomBeerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->name('user.get');
+    Route::get('/beers', GetBeersController::class)->name('beers.index');
+    Route::get('/beers/random', GetRandomBeerController::class)->name('beers.random');
+    Route::get('/beers/{beerId}', GetBeerController::class)->name('beers.show');
 });
