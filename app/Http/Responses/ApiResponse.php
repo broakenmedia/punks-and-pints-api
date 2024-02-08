5cffd08b684640813f0af267481fce2c8d46d6c5
BroakenMedia <joe@broakenmedia.co.uk>
2024-02-08 18:54:28 +0000

    Add flash message middleware for inertia and make it possible to pass additional params to the ApiResponse object.

--- app/Http/Responses/ApiResponse.php
+++ app/Http/Responses/ApiResponse.php
@@ -9,13 +9,14 @@ class ApiResponse implements Responsable
 {
     public function __construct(
         private $data = [],
+        private $extra = [],
         private int $code = Response::HTTP_OK,
     ) {
     }
 
     public function toResponse($request)
     {
-        $response = ['success' => true, 'data' => $this->data];
+        $response = array_merge(['success' => true, 'data' => $this->data], $this->extra);
 
         return response()->json($response, $this->code);
     }
