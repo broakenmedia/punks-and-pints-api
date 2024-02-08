cfb85472aafb78bdc8b120479d34d9f37ce63f0e
BroakenMedia <joe@broakenmedia.co.uk>
2024-02-08 02:53:20 +0000

    Add toast lib and some styling to help with validation.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,13 @@
 import './bootstrap';
 import '../css/app.css';
+import "vue-toastification/dist/index.css";
 
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import Toast from "vue-toastification";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,11 +15,12 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        const pinia = createPinia()
+        const pinia = createPinia();
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .use(pinia)
+            .use(Toast)
             .mount(el);
     },
     progress: {
