1ff3c0dc80b3baaf003bf8ae10e53cae36d91685
BroakenMedia <joe@broakenmedia.co.uk>
2024-02-08 01:01:11 +0000

    Add supporting JS libraries.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
 
+import { createPinia } from 'pinia'
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -12,9 +13,11 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
+        const pinia = createPinia()
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(pinia)
             .mount(el);
     },
     progress: {
