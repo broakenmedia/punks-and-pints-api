b1e4fb3422bd2cfb8b41e4c8859c1653f91cc301
BroakenMedia <joe@broakenmedia.co.uk>
2024-02-08 19:19:29 +0000

    Use the display beer to be consistent with the sourrounding items.

--- resources/js/Components/Punk/BeerDetailModal.vue
+++ resources/js/Components/Punk/BeerDetailModal.vue
@@ -100,7 +100,7 @@
 					<p class="text-xl font-bold pl-2">{{ displayBeer.name }}</p>
 				</div>
 				<div class="gap-4 flex justify-end items-center">
-					<button title="Save for later" ref="saveButton" @click="saveBeer(beer.id)" v-if="!beerSaved">
+					<button title="Save for later" ref="saveButton" @click="saveBeer(displayBeer.id)" v-if="!beerSaved">
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
 							stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-save">
 							<path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />
